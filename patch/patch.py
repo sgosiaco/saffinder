3e8586a9c3d295eeb3efc1156c606b3229db3bb8
sgosiaco <sgosiaco@me.com>
2020-12-01 04:09:31 -0800

    Added generation of diff text between old and new for easy viewing. Added comments for patch function.

--- patch/patch.py
+++ patch/patch.py
@@ -1,3 +1,4 @@
+import difflib
 import os
 import requests
 import requests_cache
@@ -6,22 +7,22 @@ def patch(saf):
     out = (saf
         .replace('Resolve', 'Will')
         .replace('Revenant', 'Spectre')
-        .replace('Spectre\'s Lucentboon', 'Lustrous Spectre')
-        .replace('Lucentrush', 'Apparition')
-        .replace('Shield of the Spectre', 'Spectre Shield') #
-        .replace('Lifesteal', 'Lifesteal Strike')
-        .replace('Lucent Strike', 'Lustrous Strike')
-        .replace('Lucent Grace', 'Luminous Grace')
-        .replace('Swift Arrows', 'Swift Arrows Strike')
-        .replace('Instant Guard Photons', 'Flashguard Lucent')
-        .replace('Impregnable Chain', 'Mighty Chain')
-        .replace('Prolonged Katana Release', 'Prolonged Blade')
-        .replace('Photon Adaptation', 'Photon V Adaptation')
-        .replace('Photonic Trap', 'Lustrous Trap')
-        .replace('Providential Refinement', 'Refined Providence')
-        .replace('Guardian Gear', 'Guardian Shield')
-        .replace('Photon Reduction', 'Photon Descent')
-        .replace('Superior Prowess', 'Skillful Adept')
+        .replace('Spectre\'s Lucentboon', 'Lustrous Spectre') #S2: Lustrous Spectre
+        .replace('Lucentrush', 'Apparition') #S2: Spectre Apparition
+        .replace('Shield of the Spectre', 'Spectre Shield') #S2: Spectre Shield
+        .replace('Lifesteal', 'Lifesteal Strike') #S4: Lifesteal Strike
+        .replace('Lucent Strike', 'Lustrous Strike') #S2: Lustrous Strike
+        .replace('Lucent Grace', 'Luminous Grace') #S3: Luminous Grace
+        .replace('Swift Arrows', 'Swift Arrows Strike') #S4: Swift Arrows Strike
+        .replace('Instant Guard Photons', 'Flashguard Lucent') #S4: Flashguard Lucent
+        .replace('Impregnable Chain', 'Mighty Chain') #S4: Mighty Chain
+        .replace('Prolonged Katana Release', 'Prolonged Blade') #S4: Prolonged Blade
+        .replace('Photon Adaptation', 'Photon V Adaptation') #S4: Photon V Adaptation
+        .replace('Photonic Trap', 'Lustrous Trap') #S4: Lustrous Trap
+        .replace('Providential Refinement', 'Refined Providence') #S4: Refined Providence
+        .replace('Guardian Gear', 'Guardian Shield') #S3: Guardian Shield
+        .replace('Photon Reduction', 'Photon Descent') #S1: Photon Descent
+        .replace('Superior Prowess', 'Skillful Adept') #S2: Skillful Adept
         .replace('Doom Break', 'Doom Break I')
     )
     return out
@@ -48,16 +49,23 @@ weapons = [
 
 requests_cache.install_cache('cache')
 try:
+    os.mkdir('diff')
+except FileExistsError:
+    print('diff directory exists')
+try:
     os.mkdir('old')
+except FileExistsError:
+    print('old directory exists')
+try:
     os.mkdir('new')
 except FileExistsError:
-    print('Directories exist')
+    print('new directory exists')
 cwd = os.getcwd()
 
 for weapon in weapons:
     print(f'Processing {weapon}')
     page = requests.get(f'https://pso2na.arks-visiphone.com/index.php?title={weapon}&action=edit')
-    info = page.content.decode(encoding='utf-8') #.split('{{Weapon_Listing}}')[1].replace(u'\u2605', '*').replace('&lt;/table>', '').replace('&amp;', '&')
+    info = page.content.decode(encoding='utf-8')
     start = info.find('{{Weapon_Listing}}')
     end = info.find('{{Weapon_Listing}}', start + 1, -1)
     info = info[start:end+len('{{Weapon_Listing}}')]
@@ -69,3 +77,8 @@ for weapon in weapons:
     
     with open(os.path.join(cwd, 'old', f'{weapon}.txt'), 'w', encoding='utf-8') as old:
         old.write(info)
+
+    diffText = difflib.ndiff(info.splitlines(keepends=True), newpage.splitlines(keepends=True))
+
+    with open(os.path.join(cwd, 'diff', f'{weapon}.txt'), 'w', encoding='utf-8') as diff:
+        diff.write(''.join(line for line in diffText if line.startswith('- ') or line.startswith('? ') or line.startswith('+ ')))
